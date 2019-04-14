a27b3a183dbdb7ca23f4cea98b276c6d2f491778
Yue Chu Cheng <maggie9907@gmail.com>
2019-04-14 18:38:43 +0800

    nav-bar sketch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 import $ from 'jquery';
 import '../css/style.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../node_modules/normalize.css/normalize.css";
