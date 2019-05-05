0a0d533ac1758fb8c7ceb724d6513ce7c3d90bce
Yue Chu Cheng <maggie9907@gmail.com>
2019-05-05 23:41:28 +0800

    add animate

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,12 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../../node_modules/normalize.css/normalize.css";
 import tree from"../images/tree.png";
+import bgi from"../images/bg.png";
 
+import cloud from "../images/cloud.png";
+
+
+$(".main_pic").attr("src", bgi);
+
+$(".cloud").attr("src", cloud);
 $("#tree").attr("src", tree);
