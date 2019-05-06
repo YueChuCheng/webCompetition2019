0a2fdc009bd0aa27f514eb85d45a1e9728e64074
Yue Chu Cheng <maggie9907@gmail.com>
2019-05-06 22:26:16 +0800

    add animate

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,34 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../../node_modules/normalize.css/normalize.css";
 import tree from"../images/tree.png";
 import bgi from"../images/bg.png";
-
+import bird from "../images/bird.png";
 import cloud from "../images/cloud.png";
 import g_pa from "../images/grandpa.png";
+
+import f_pa from "../images/fruit/P.png";
+import f_ct from "../images/fruit/CT.png";
+import f_df from "../images/fruit/DF.png";
+import f_g from "../images/fruit/G.png";
+import f_javaA from "../images/fruit/javaA.png";
+import f_juju from "../images/fruit/juju.png";
+import f_L_CT from "../images/fruit/L-CT.png";
+import f_litchi from "../images/fruit/litchi.png";
+import f_peach from "../images/fruit/peach.png";
+import f_longan from "../images/fruit/longan.png";
 
 $(".main_pic").attr("src", bgi);
 $(".cloud").attr("src", cloud);
 $("#tree").attr("src", tree);
 $(".g-pa").attr("src", g_pa);
+$(".bird").attr("src", bird);
+
+$(".animate-f1").attr("src", f_pa);
+$(".animate-f2").attr("src", f_peach);
+$(".animate-f3").attr("src", f_ct);
+$(".animate-f4").attr("src", f_litchi);
+$(".animate-f5").attr("src", f_L_CT);
+$(".animate-f6").attr("src", f_df);
+$(".animate-f7").attr("src", f_javaA);
+$(".animate-f8").attr("src", f_g);
+$(".animate-f9").attr("src", f_longan);
+$(".animate-f10").attr("src", f_juju);
