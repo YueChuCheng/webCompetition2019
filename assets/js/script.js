f24d4ae420db26828c2797198aebfff516a0b88b
Yue Chu Cheng <maggie9907@gmail.com>
2019-05-16 17:56:21 +0800

    add main_story

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,9 @@ import f_longan from "../images/fruit/longan.png";
 import nav_logo from "../images/nav_logo.png";
 import title_logo from "../images/title_logo.png";
 
+import rainbow from "../images/main_story/RB.png"
+import city from "../images/main_story/city.png"
+
 $(".title_logo").attr("src", title_logo);
 $(".nav_logo").attr("src", nav_logo);
 
@@ -40,3 +43,6 @@ $(".animate-f7").attr("src", f_javaA);
 $(".animate-f8").attr("src", f_g);
 $(".animate-f9").attr("src", f_longan);
 $(".animate-f10").attr("src", f_juju);
+
+$(".RB").attr("src", rainbow);
+$(".city").attr("src", city);
